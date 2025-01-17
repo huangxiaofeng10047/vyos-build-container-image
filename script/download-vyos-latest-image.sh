efd4342e527a0cdf2761039c0970fcdb9597ff15
CoderHuangxf <hxf168482@gmail.com>
2025-01-17 15:33:20 +0800

    Update download-vyos-latest-image.sh

--- script/download-vyos-latest-image.sh
+++ script/download-vyos-latest-image.sh
@@ -11,5 +11,5 @@ download_url=$(echo "$release_info" | jq -r '.assets[0].browser_download_url')
 
 tag_name=$(echo "$release_info" | jq -r '.tag_name' | rev | cut -c 5- | rev)
 echo "tag_name=$tag_name" >> $GITHUB_ENV
-
+echo "$download_url"
 curl -L -o vyos-rolling-latest.iso "$download_url"
